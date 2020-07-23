52ed94d00f23d4e765e08f83014739dadeab11f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-23 06:43:33 +0000

    Deploy Aman02-github/Frontend-SE to github.com/Aman02-github/Frontend-SE.git:gh-pages

---
+++ build/precache-manifest.f1c823a5333f3414971a3758892c9616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3833d7bed26f38b1255cd9ea107955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa4d42b6c1076b9a43c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa4d42b6c1076b9a43c",
+    "url": "/static/js/main.c206d71f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
